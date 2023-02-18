de35afcc7e593005562b74b8f09f3f1c9ceae662
iMiku <lalescum2002@gmail.com>
2023-02-19 00:56:57 +0200

    added angular routing and some other components

--- car_wm/src/app/app.module.ts
+++ car_wm/src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarsPageComponent } from './cars-page/cars-page.component';
+import { AccountPageComponent } from './account-page/account-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    CarsPageComponent,
+    AccountPageComponent,
+    AdminPageComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomePageComponent },
+      { path: 'cars', component: CarsPageComponent },
+      { path: 'account', component: AccountPageComponent },
+      { path: 'admin', component: AdminPageComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', component: ErrorPageComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
